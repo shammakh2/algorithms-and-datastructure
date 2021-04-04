efbb54bfb0f9fc01e9951a83798d293ff4d0f98d
Shammakh Naseer <shammakh_n@hotmail.com>
2021-04-04 20:42:06 +0100

    implemented addEdge function for Graph and modified Node class for edge insertion

--- Graphs/Node.py
+++ Graphs/Node.py
@@ -3,26 +3,63 @@ class Node:
 
     def __init__(self):
         self.id = None
-        self.toNode = {}
+        self.__toNode = {}
+
+    def addToNode(self, node):
+        if (self.__toNode.get(node.id) is None):
+            self.__toNode[node.id] = node
+            return True
+        else:
+            return False
 
 
 class Graph:
 
     def __init__(self):
-        self.nodes = {}
+        self.__nodes = {}
 
-    def addNode(node):
+    def addNode(self, node):
         pass
 
-    def addEdge(fromNode, toNode, strict):
+    def addEdge(self, fromNode, toNode):
+
+        nfrom = self.__nodes.get(fromNode)
+        nto = self.__nodes.get(toNode)
+
+        if (nfrom is None):
+            nfrom = Node()
+            nfrom.id = fromNode
+            self.__nodes[fromNode] = nfrom
+
+        if (nto is None):
+            nto = Node()
+            nto.id = toNode
+            self.__nodes[toNode] = nto
+
+        check1 = nfrom.addToNode(nto)
+        check2 = nto.addToNode(nfrom)
+
+        if (not check1 and not check2):
+            return False
+        elif (not check1 or not check2):
+            # raise CorruptDataException("Data corruption detected")
+            return
+
+        return True
+
+    def addEdgeStrict(self, fromNode, toNode):
         pass
 
-    def depthFirstSearch():
+    def depthFirstSearch(self):
         pass
 
-    def breadthFirstSearch():
+    def breadthFirstSearch(self):
         pass
 
 
 if __name__ == "__main__":
-    print(hasattr(Node(), "id"))
+    x = Graph()
+    x.addEdge("jeff", "chu")
+    print(x._Graph__nodes)
+    print(x._Graph__nodes["jeff"]._Node__toNode)
+    print(x._Graph__nodes["chu"]._Node__toNode)
