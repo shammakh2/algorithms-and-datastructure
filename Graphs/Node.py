27c4b1f8c6bb9dbf581fe63497cce7256345a562
Shammakh Naseer <shammakh_n@hotmail.com>
2021-04-04 17:58:10 +0100

    Creating Graph class method templates

--- Graphs/Node.py
+++ Graphs/Node.py
@@ -6,5 +6,23 @@ class Node:
         self.toNode = {}
 
 
+class Graph:
+
+    def __init__(self):
+        self.nodes = {}
+
+    def addNode(node):
+        pass
+
+    def addEdge(fromNode, toNode, strict):
+        pass
+
+    def depthFirstSearch():
+        pass
+
+    def breadthFirstSearch():
+        pass
+
+
 if __name__ == "__main__":
     print(hasattr(Node(), "id"))
